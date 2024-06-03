dd75a00d15a345e95fc3e292dffccc362e01fbc4
Kunal <kunalukey32@gmail.com>
2024-06-03 16:36:27 +0530

    updated unused vars

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 
-type Props = {};
-
-const Footer = (props: Props) => {
+const Footer = () => {
   return (
     <div className="w-full mt-auto">
       <footer className="footer p-10 bg-base-200 text-base-content">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 
-type Props = {};
-
-function Navbar({}: Props) {
+function Navbar() {
   return (
     <div className="navbar bg-base-100 shadow-md py-4">
       <Link to="/" className="btn btn-ghost text-xl">
